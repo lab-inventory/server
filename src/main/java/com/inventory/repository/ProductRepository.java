b37cff5be30ab5cb7efec32d3bf358a20aa0dc7e
edgararthur <gilfoylearthur@protonmail.com>
2023-03-09 10:46:50 +0000

    create a java interface for the product repository

---
+++ src/main/java/com/inventory/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.inventory.repository;
+
+import com.inventory.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
