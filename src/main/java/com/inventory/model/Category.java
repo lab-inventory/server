e579c284c3d0eb171414c398a2d550cb88c1550c
edgararthur <gilfoylearthur@protonmail.com>
2023-03-29 19:32:20 +0000

    changed naming convention of items

---
+++ src/main/java/com/inventory/model/Category.java
@@ -0,0 +1,60 @@
+package com.inventory.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "cid")
+	private String cid;
+	@Column(name = "name", nullable = false)
+	private String name;
+	@Column(name = "description", nullable = true)
+	private String description;
+
+	public Category() {
+		super();
+	}
+	public Category(int id, String cid, String name, String description) {
+		this.id = id;
+		this.cid = cid;
+		this.name = name;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public  String getCid() {
+		return cid;
+	}
+
+	public  void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
