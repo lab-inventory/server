d9cf713d405d40aaf6336f8c2faefe8cc5dbbf38
Edward Edgar Arthur <108295488+edgararthur@users.noreply.github.com>
2023-03-31 13:47:38 +0000

    refactored if block

--- src/main/java/com/inventory/structures/BubbleSortStruct.java
+++ src/main/java/com/inventory/structures/BubbleSortStruct.java
@@ -7,10 +7,10 @@ public class BubbleSortStruct {
 		for (int i = 0; i < categorySize - 1; i++) {
 			for (int j = 0; j < categorySize - i - 1; j++) {
 				if (compare(categories.get(j), categories.get(j + 1)) > 0) {
-                    Category temp = categories.get(j);
-                    categories.set(j, categories.get(j + 1));
-                    categories.set(j + 1, temp);
-                }
+                    			Category temp = categories.get(j);
+                    			categories.set(j, categories.get(j + 1));
+                    			categories.set(j + 1, temp);
+                		}
 			}
 		}	
 	}
